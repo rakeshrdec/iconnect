a973c3ae940996a38da16594b1dfa72e9e77c5ed
sam <sam.iet2006@gmail.com>
2023-03-05 15:06:16 +0530

    update UI

--- src/screens/login/login.js
+++ src/screens/login/login.js
@@ -102,6 +102,7 @@ const Login = ({ navigation }) => {
                     }
                   />
 
+
                   {/* uiKITTEN */}
                   {/* <Input
                       value={loginId}
