ce1bec8e662ead944639c8488cb568e8925ab27c
sanjaysingh-iroc <sam.iet2006@gmail.com>
2023-03-16 09:19:26 +0530

    Add Student Original image

--- src/screens/student/studentCard.js
+++ src/screens/student/studentCard.js
@@ -37,50 +37,53 @@ const StudentCard = ({navigation}) =>{
     // const studentList = [1,2,3]
     const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window')
     // const studentList = [{name:'rakesh kumar mishra', rollno:'12', class:'One(A)', mobile:'9891979411'},{name:'Ayansh', rollno:'12', class:'Six(A)', mobile:'9891979411'},{name:'sonu kumar', rollno:'2', class:'One(A)', mobile:'9891979411'}]
-    const [studentList,setStudentList] = useState([]);
+    const [studentList, setStudentList] = useState([]);
 
-    const gotoNextPage = (e) =>{
+    const gotoNextPage = (e) => {
         actions.selectedStudentDetails(e)
         navigation.navigate('HomePage');
     }
-    return(
+    return (
         <>
-        <SafeAreaView style={{flex:1}}>
-            <View style={{
-                width: SCREEN_WIDTH,
-                height: 0,
-                borderTopColor: "#2E4AA0",
-                borderTopWidth: SCREEN_HEIGHT ,
-                borderRightWidth: SCREEN_WIDTH+30,
-                borderRightColor: '#F0BA19'
-            }}>
-            </View>
-            {/* <Text style={{color:'white',position:'absolute'}}>HIIIIII</Text> */}
-            {/* <View style={{backgroundColor:'red',height:100,borderWidth:1,position:'absolute'}}>
+            <SafeAreaView style={{ flex: 1 }}>
+                <View style={{
+                    width: SCREEN_WIDTH,
+                    height: 0,
+                    borderTopColor: "#2E4AA0",
+                    borderTopWidth: SCREEN_HEIGHT,
+                    borderRightWidth: SCREEN_WIDTH + 30,
+                    borderRightColor: '#F0BA19'
+                }}>
+                </View>
+                {/* <Text style={{color:'white',position:'absolute'}}>HIIIIII</Text> */}
+                {/* <View style={{backgroundColor:'red',height:100,borderWidth:1,position:'absolute'}}>
                 <Text style={{color:'white', fontSize:25, alignSelf:'center'}}>Home</Text>
             </View> */}
-                <View style={{height:55,backgroundColor:'#2E4AA0',justifyContent:'center',position:'absolute',width:'100%'}}>
-                    <Text style={{color:'white', fontSize:20, alignSelf:'center'}}>Student List</Text>
+                <View style={{ height: 55, backgroundColor: '#2E4AA0', justifyContent: 'center', position: 'absolute', width: '100%' }}>
+                    <Text style={{ color: 'white', fontSize: 20, alignSelf: 'center' }}>Student List</Text>
                 </View>
-                <ScrollView style={{position:'absolute',width:'100%',marginTop:70}}>
-                    {studentList.map((e,i)=>(
-                        <Pressable 
-                        onPress={()=>{
-                            gotoNextPage(e);
-                        }}
-                        style={{elevation:10,backgroundColor:'white', minHeight:120, margin:10, borderRadius:15,padding:5,alignItems:'center'}}>
-                            <View style={{flex:1,margin:10, flexDirection:'row',alignItems:'center'}}>
-                                <View style={{flex:1,  justifyContent:'center',alignItems:'center'}}>
-                                    <Image source={require('../../../assets/logo/male_student_avatar.png')} style={{width: 70,height: 70,resizeMode: 'stretch', borderRadius:150}}/>
+                <ScrollView style={{ position: 'absolute', width: '100%', marginTop: 70 }}>
+                    {studentList.map((e, i) => (
+                        <Pressable
+                            onPress={() => {
+                                gotoNextPage(e);
+                            }}
+                            style={{ elevation: 10, backgroundColor: 'white', minHeight: 120, margin: 10, borderRadius: 15, padding: 5, alignItems: 'center' }}>
+                            <View style={{ flex: 1, margin: 10, flexDirection: 'row', alignItems: 'center' }}>
+                                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                                    <Image source={require('../../../assets/logo/male_student_avatar.png')} style={{ width: 70, height: 70, resizeMode: 'stretch', borderRadius: 150 }} />
+                                    <Image source={{ uri: 'http://13.127.128.192:8081/' + e.uploadImg }} style={{ width: 70, height: 70, resizeMode: 'stretch', borderRadius: 150 }} />
+                                  
+
                                 </View>
-                                <View style={{flex:2, }}>
-                                    <Text style={{color:'black', fontWeight:'bold'}}>{e.name}</Text>
-                                    <Text style={{color:'black', fontWeight:'bold'}}>Roll No : {e.id}</Text>
-                                    <Text style={{color:'black', fontWeight:'bold',}}>Mobile : {e.mobile} </Text>
+                                <View style={{ flex: 2, }}>
+                                    <Text style={{ color: 'black', fontWeight: 'bold' }}>{e.name}</Text>
+                                    <Text style={{ color: 'black', fontWeight: 'bold' }}>Roll No : {e.id}</Text>
+                                    <Text style={{ color: 'black', fontWeight: 'bold', }}>Mobile : {e.mobile} </Text>
                                 </View>
                             </View>
                         </Pressable>
-                    ))} 
+                    ))}
                 </ScrollView>
             </SafeAreaView>
         </>
