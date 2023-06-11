7cc8955d6f9b966c511c7b681bc43d5215710e55
Rakesh Mishra <rm799523@gmail.com>
2023-06-11 16:15:02 +0530

    Changeapp and package name and remove newarchitecture folder

--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/schoolapp/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/school/iclass/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
