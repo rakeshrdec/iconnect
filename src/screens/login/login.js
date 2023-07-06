b90449fe5b8fd628fa50bd5e27901465736edc49
sam <sam.iet2006@gmail.com>
2023-07-06 17:00:47 +0530

    SSD

--- src/screens/login/login.js
+++ src/screens/login/login.js
@@ -78,13 +78,13 @@ const Login = ({ navigation }) => {
         />
       </View>
       <View style={{ flex: 1, backgroundColor: '#2E4AA0', justifyContent: 'center', }}>
-        <Text style={{ color: 'white', fontWeight: 'bold', textAlign: 'center', fontSize: 20, margin: 0 }}>Krishna Public  School</Text>
-        <Text style={{ color: 'white', fontWeight: 'bold', textAlign: 'center', fontSize: 20, margin: 0, paddingBottom: 10 }}> Thana Dariyaoganj</Text>
+        <Text style={{ color: 'white', fontWeight: 'bold', textAlign: 'center', fontSize: 30, margin: 0 }}>St. S. D. Memorial School</Text>
+        <Text style={{ color: 'white', fontWeight: 'bold', textAlign: 'center', fontSize: 20, margin: 0, paddingBottom: 10 }}>Rehna Nala, Bihari Nagar, Firozabad</Text>
 
         <View style={{ backgroundColor: 'white', width: '95%', alignSelf: 'center', borderRadius: 15, minHeight: 450, paddingTop: 5 }}>
           {/* logo section */}
           <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-            <Image source={require('../../../assets/logo/applogo.png')} style={{ width: 100, height: 100, borderRadius: 15, resizeMode: 'stretch', alignSelf: 'center' }} />
+            <Image source={require('../../../assets/logo/applogo.png')} style={{ width: 100, height: 100, alignSelf: 'center' }} />
           </View>
           {/* login section */}
           <View style={{ borderColor: '#F0BA19', width: '100%', justifyContent: 'center', padding: 10, borderRadius: 20, }}>
